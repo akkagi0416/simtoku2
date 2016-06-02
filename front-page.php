f0389daf1d3c165ba3701d81b17ac117f8e7ff55
akkagi0416 <akkagi0416@gmail.com>
2016-06-02 19:19:33 +0900

    add front-page.php

---
+++ front-page.php
@@ -0,0 +1,37 @@
+<?php get_header(); ?>
+  <main>
+    <?php if( have_posts() ) : ?>
+      <?php while( have_posts() ) : the_post(); ?>
+        <article class="page <?php echo $class; ?>">
+          <h1 class="title"><?php the_title(); ?></h1>
+          <?php the_content(); ?>
+        </article>
+      <?php endwhile; ?>
+    <?php endif; ?>
+    <?php
+      $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+      $args = array(
+          'posts_per_page' => 6,
+          'paged' => $paged,
+      );
+      $my_query = new WP_Query( $args );
+    ?>
+    <section class="frontpage">
+      <h2>新着記事</h2>
+      <div class="topics">
+        <?php if( $my_query->have_posts() ) : ?>
+          <?php while( $my_query->have_posts() ) : $my_query->the_post(); ?>
+            <?php
+              $title     = get_the_title();
+              $permalink = get_the_permalink();
+              $datetime  = get_the_time( 'Y-m-d' );
+              $thumbnail = get_the_post_thumbnail( $post->ID, 'thumbnail' );
+              echo make_topic( '', $title, $permalink, $datetime, $thumbnail );
+            ?>
+          <?php endwhile; ?>
+        <?php endif; ?>
+      </div>
+    </section>
+  </main>
+  <?php get_sidebar(); ?>
+<?php get_footer(); ?>
