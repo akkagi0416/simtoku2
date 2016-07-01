0f001ce227da25a0e03a8b4a7b24539b54d98196
akkagi0416 <akkagi0416@gmail.com>
2016-07-01 14:15:30 +0900

    adjust sns button

--- single.php
+++ single.php
@@ -10,6 +10,7 @@
           <time class="postdate" datetime="<?php the_time('Y-m-d'); ?>"><i class="fa fa-clock-o fa-fw" aria-hidden="true"></i><?php the_time('Y-m-d'); ?></time>
           <?php the_content(); ?>
         </article>
+        <?php sns_button(); ?>
         <section class="relation">
           <h2>関連記事</h2>
           <?php relation( $post ); ?>
@@ -18,7 +19,6 @@
       <?php endwhile; ?>
     <?php endif; ?>
 
-    <?php sns_button(); ?>
   </main>
   <?php get_sidebar(); ?>
 <?php get_footer(); ?>
