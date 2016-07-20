24765b87f250f43042a14c10ddbfa043a5d1b62d
akkagi0416 <akkagi0416@gmail.com>
2016-07-20 11:24:37 +0900

    remove topics_news from frontpage.php because of making shortcode

--- front-page.php
+++ front-page.php
@@ -10,31 +10,6 @@
         </article>
       <?php endwhile; ?>
     <?php endif; ?>
-    <?php
-      $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-      $args = array(
-          'posts_per_page' => 6,
-          'paged' => $paged,
-      );
-      $my_query = new WP_Query( $args );
-    ?>
-    <section class="frontpage">
-      <h2>新着記事</h2>
-      <div class="topics">
-        <?php if( $my_query->have_posts() ) : ?>
-          <?php while( $my_query->have_posts() ) : $my_query->the_post(); ?>
-            <?php
-              $title     = get_the_title();
-              $permalink = get_the_permalink();
-              $datetime  = get_the_time( 'Y-m-d' );
-              $thumbnail = get_the_post_thumbnail( $post->ID, 'thumbnail' );
-              echo make_topic( '', $title, $permalink, $datetime, $thumbnail );
-            ?>
-          <?php endwhile; ?>
-        <?php endif; ?>
-      </div>
-      <p class="to_category"><a href="<?php echo home_url( '/singles' ); ?>"><i class="fa fa-arrow-right"></i>他の新着記事一覧</a></p>
-    </section>
 
     <?php sns_button(); ?>
   </main>
